9e15fbcd63a0986b277e6d3fc82f2087940b47f1
Ismael <ismaeldev96@gmail.com>
2023-07-18 21:18:44 -0300

    atribuindo get e set

--- atividade2Aula3.js
+++ atividade2Aula3.js
@@ -12,6 +12,43 @@ class Carro {
         this.acentos = acentos;
     }
 
+    set _modelo(modelo){
+        this.modelo = modelo;
+    }
+    get _modelo(){
+        return this.modelo;
+    }
+    set _ano(ano){
+        this.ano = ano;
+    }
+    get _ano(){
+        return this.ano;
+    }
+    set _valor(valor){
+        this.valor = valor;
+    }
+    get _valor(){
+        return this.valor;
+    }
+    set _cor(cor){
+        this.cor = cor;
+    }
+    get _cor(){
+        return this.cor;
+    }
+    set _quantidadePortas(quantidadePortas){
+        this.quantidadePortas = quantidadePortas;
+    }
+    get _quantidadePortas(){
+        return this.quantidadePortas;
+    }
+    set _acentos(acentos){
+        this.acentos = acentos;
+    }
+    get _acentos(){
+        return this.acentos;
+    }
+
     Ligar(){
         console.log(`Ligando o ${this.modelo}/${this.ano} -- VRUM VRUM VRUM`);
     }
@@ -22,17 +59,16 @@ class Carro {
     }
 
     DescricaoVeiculo(){
-        console.log(`Modelo: ${this.modelo}`);
-        console.log(`Ano: ${this.ano}`);
-        console.log(`Valor: ${this.valor}`);
-        console.log(`Cor: ${this.cor}`);
-        console.log(`Portas: ${this.quantidadePortas}`);
-        console.log(`Lugares: ${this.acentos}`);
-        console.log(`Idade do veiculo: ${this.Idade()} anos`);
+        console.log(`Modelo: ${this.modelo}\n`);
+        console.log(`Ano: ${this.ano}\n`);
+        console.log(`Valor: ${this.valor.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}\n`);
+        console.log(`Cor: ${this.cor}\n`);
+        console.log(`Portas: ${this.quantidadePortas}\n Lugares: ${this.acentos}\n Idade do veiculo: ${this.Idade()} anos\n`);
     }
 
 }
 
 var primeiroCarro = new Carro("Etios 1.6", 2014, 38000.00, "Verde", 4, 5);
+primeiroCarro.modelo = "Opala";
 primeiroCarro.DescricaoVeiculo();
-primeiroCarro.Ligar();
+
